74301de84807f8703959ad79941f4ffbd9f144fc
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-22 00:22:27 +0400

    Finished GameState class and its methods

--- GoFish/GameState.cs
+++ GoFish/GameState.cs
@@ -39,7 +39,10 @@
         /// <param name="currentPlayer">The current player</param>
         /// <returns>A random player that the current player can ask for a card</returns>
         public Player RandomPlayer(Player currentPlayer) =>
-            throw new NotImplementedException();
+            Players
+                .Where(player => player != currentPlayer)
+                .Skip(Player.Random.Next(Players.Count() - 1))
+                .First();
 
         /// <summary>
         /// Makes one player play a round
@@ -52,7 +55,32 @@
         public string PlayRound(Player player, Player playerToAsk,
         Values valueToAskFor, Deck stock)
         {
-            throw new NotImplementedException();
+            var valuePlural = (valueToAskFor == Values.Six) ? "Sixes" : $"{valueToAskFor}s";
+            var message = $"{player.Name} asked {playerToAsk.Name} for {valuePlural}{Environment.NewLine}";
+
+            var cards = playerToAsk.DoYouHaveAny(valueToAskFor, stock);
+            if (cards.Count() > 0)
+            {
+                player.AddCardsAndPullOutBooks(cards);
+                message += $"{playerToAsk.Name} has {cards.Count()} {valueToAskFor} card{Player.S(cards.Count())}";
+            }
+            else if (stock.Count == 0)
+            {
+                message += $"The stock is out of cards";
+            }
+            else
+            {
+                player.DrawCard(stock);
+                message += $"{player.Name} drew a card";
+            }
+
+            if (player.Hand.Count() == 0)
+            {
+                player.GetNextHand(stock);
+                message += $"{Environment.NewLine}{player.Name} ran out of cards," +
+                    $" drew {player.Hand.Count()} from the stock";
+            }
+            return message;
         }
 
         /// <summary>
@@ -62,7 +90,15 @@
         /// <returns>A string with the winners, an empty string if there are no winners</returns>
         public string CheckForWinner()
         {
-            throw new NotImplementedException();
+            var playerCards = Players.Select(player => player.Hand.Count()).Sum();
+            if (playerCards > 0) return "";
+
+            GameOver = true;
+
+            var winningBookCount = Players.Select(player => player.Books.Count()).Max();
+            var winners = Players.Where(player => player.Books.Count() == winningBookCount);
+            if (winners.Count() == 1) return $"The winner is {winners.First().Name}";
+            return $"The winners are {string.Join(" and ", winners)}";
         }
     }
 }
