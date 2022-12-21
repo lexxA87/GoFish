19e9a05f2482a242e4083a80cf162724ef957e3b
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-22 01:06:18 +0400

    Added the skeleton Program class

--- GoFish/Program.cs
+++ GoFish/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Play a game of Go Fish!
+        /// </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        /// <summary>
+        /// The GameController to manage the game
+        /// </summary>
+        static GameController gameController;
+
+        /// <summary>
+        /// Prompt the human player for a card value
+        /// in their hand
+        /// </summary>
+        /// <returns>The value to ask for</returns>
+        static Values PromptForAValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Prompt the human player for an opponent
+        /// to ask for a card
+        /// </summary>
+        /// <returns>The opponent to ask</returns>
+        static Player PromptForAnOpponent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
