ff4d1892db89d626fc98207601e177a5c2fd3787
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-22 00:38:07 +0400

    Added the skeleton for the GameController class from the book

---
+++ GoFish/GameController.cs
@@ -0,0 +1,51 @@
+﻿namespace GoFish
+{
+    public class GameController
+    {
+        public static Random Random = new Random();
+
+        public GameState gameState;
+        public bool GameOver { get { return gameState.GameOver; } }
+        public Player HumanPlayer { get { return gameState.HumanPlayer; } }
+        public IEnumerable<Player> Opponents { get { return gameState.Opponents; } }
+
+        public string Status { get; private set; }
+
+        /// <summary>
+        /// Constructs a new GameController
+        /// </summary>
+        /// <param name="humanPlayerName">Name of the human player</param>
+        /// <param name="computerPlayerNames">Names of the computer players</param>
+        public GameController(string humanPlayerName, IEnumerable<string> computerPlayerNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Plays the next round, ending the game if everyone ran out of cards
+        /// </summary>
+        /// <param name="playerToAsk">Which player the human is asking for a card</param>
+        /// <param name="valueToAskFor">The value of the card the human is asking for</param>
+        public void NextRound(Player playerToAsk, Values valueToAskFor)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// All of the computer players that have cards play the next round. If the human is
+        /// out of cards, then the deck is depleted and they play out the rest of the game.
+        /// </summary>
+        private void ComputerPlayersPlayNextRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Starts a new game with the same player names
+        /// </summary>
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
