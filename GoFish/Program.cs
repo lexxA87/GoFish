680806646572a4b32540feaa108a05699ff8d102
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-22 01:16:37 +0400

    Finished the Program class

--- GoFish/Program.cs
+++ GoFish/Program.cs
@@ -7,7 +7,42 @@
         /// </summary>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name: ");
+            var humanName = Console.ReadLine();
+
+            Console.Write("Enter the number of computer opponents: ");
+            int opponentCount;
+            while (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out opponentCount)
+            || opponentCount < 1 || opponentCount > 4)
+            {
+                Console.WriteLine("Please enter a number from 1 to 4");
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Welcome to the game, {humanName}");
+
+
+            gameController = new GameController(humanName,
+            Enumerable.Range(1, opponentCount).Select(i => $"Computer #{i}"));
+            Console.WriteLine(gameController.Status);
+
+            while (!gameController.GameOver)
+            {
+                while (!gameController.GameOver)
+                {
+                    Console.WriteLine($"Your hand:");
+                    foreach (var card in gameController.HumanPlayer.Hand
+                    .OrderBy(card => card.Suit)
+                    .OrderBy(card => card.Value))
+                        Console.WriteLine(card);
+                    var value = PromptForAValue();
+                    var player = PromptForAnOpponent();
+                    gameController.NextRound(player, value);
+                    Console.WriteLine(gameController.Status);
+                }
+                Console.WriteLine("Press Q to quit, any other key for a new game.");
+                if (Console.ReadKey(true).KeyChar.ToString().ToUpper() == "N")
+                    gameController.NewGame();
+            }
         }
 
         /// <summary>
@@ -22,7 +57,16 @@
         /// <returns>The value to ask for</returns>
         static Values PromptForAValue()
         {
-            throw new NotImplementedException();
+            var handValues = gameController.HumanPlayer.Hand.Select(card => card.Value).ToList();
+            Console.Write("What card value do you want to ask for? ");
+            while (true)
+            {
+                if (Enum.TryParse(typeof(Values), Console.ReadLine(), out var value) &&
+                handValues.Contains((Values)value))
+                    return (Values)value;
+                else
+                    Console.WriteLine("Please enter a value in your hand.");
+            }
         }
 
         /// <summary>
@@ -32,7 +76,18 @@
         /// <returns>The opponent to ask</returns>
         static Player PromptForAnOpponent()
         {
-            throw new NotImplementedException();
+            var opponents = gameController.Opponents.ToList();
+            for (int i = 1; i <= opponents.Count(); i++)
+                Console.WriteLine($"{i}. {opponents[i - 1]}");
+            Console.Write("Who do you want to ask for a card? ");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int selection)
+                && selection >= 1 && selection <= opponents.Count())
+                    return opponents[selection - 1];
+                else
+                    Console.Write($"Please enter a number from 1 to {opponents.Count()}: ");
+            }
         }
     }
 }
